7e1d0527e7008cc11a7853d099c52d2efdb12f12
Vanessa Berardozzi <ness.berardozzi@gmail.com>
2024-06-18 18:35:55 +0200

    chore: Update Swagger documentation and API title and description

--- docs/swagger.js
+++ docs/swagger.js
@@ -4,20 +4,20 @@ const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 
 const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'Quiz\'O\'Tron API',
-      version: '1.0.0',
-      description: 'A Express Quiz API',
-    },
-    servers: [
-      {
-        url: 'https://game-o-quiz-9db3a1fb3532.herokuapp.com/',
-      },
-    ],
-  },
-  apis: ['./app/routers/*.js', './app/models/*.js', './app/controllers/*.js']
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Game-o-Quiz API',
+			version: '1.0.0',
+			description: 'A Express Quiz API',
+		},
+		servers: [
+			{
+				url: 'https://quiz.berardozzi.fr/',
+			},
+		],
+	},
+	apis: ['./app/routers/*.js', './app/models/*.js', './app/controllers/*.js'],
 };
 
 const swaggerSpecs = swaggerJsDoc(options);
